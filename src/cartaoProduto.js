3aa45f50632e490f1e70ee2e6b775f13e7c4d69a
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-09-04 19:15:25 -0300

    finished aula3 :sparkles:

---
+++ src/cartaoProduto.js
@@ -0,0 +1,26 @@
+import { adicionarAoCarrinho } from "./menuCarrinho";
+import { catalogo } from "./utilidades";
+
+export function renderizarCatalogo() {
+  for (const produtoCatalago of catalogo) {
+    
+    const cartaoProduto = `<div class="border-solid w-48 m-2 flex flex-col p-2 justify-between group shadow-xl shadow-slate-400 rounded-lg" id="card-produto-${produtoCatalago.id}">
+    <div class="w-full h-full overflow-hidden rounded-lg">
+      <img
+        src="./assets/img/${produtoCatalago.imagem}"
+        alt="Porduto 1 do magazine Hashtag"
+        class="group-hover:scale-110 duration-500 origin-top-left w-full h-full object-cover cursor-pointer"
+      />
+    </div>
+      <p class="text-sm">${produtoCatalago.nome}</p>
+      <p class="text-sm">${produtoCatalago.marca}</p>
+      <p class="text-sm">${produtoCatalago.preco}</p>
+      <button id="adicionar-${produtoCatalago.id}" class="bg-slate-950 hover:bg-slate-700 duration-500 text-slate-200"><i class="fa-solid fa-cart-plus"></i></button>
+    </divc> `;
+    document.querySelector("#container-produto").innerHTML += cartaoProduto;
+    document.getElementById(`adicionar-${produtoCatalago.id}`)
+  }
+  for(const produtoCatalago of catalogo ){
+    document.getElementById(`adicionar-${produtoCatalago.id}`).addEventListener("click", ()=>adicionarAoCarrinho(produtoCatalago.id));
+  }
+}
--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,3 +1,5 @@
+import { catalogo } from "./utilidades";
+
 function abrirCarrinho() {
   document.getElementById("carrinho").classList.add("right-[0px]");
   document.getElementById("carrinho").classList.remove("right-[-360px]");
@@ -15,5 +17,26 @@ export function inicializarCarrinho() {
   botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
   botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
 }
-inicializarCarrinho();
+export function adicionarAoCarrinho(idProduto){
+  const produto = catalogo.find(p =>p.id ===idProduto);
+  const containerProdutoCarrinho = document.getElementById('produtos-carrinho');
 
+  const cartaoProdutoCarrinho = `<article class="flex bg-slate-100 rounded-lg p-1 relative">
+  <button id="fechar-carrinho" class="absolute top-0 right-2">
+    <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i>
+  </button>
+  <img
+    src="./assets/img/${produto.imagem}"
+    alt="Carrinho: ${produto.nome}"
+    title="Carrinho: Camisa Larga com Bolsos"
+    class="h-24 rounded-lg"
+  />
+  <div class="py-2">
+    <p class="text-slate-900 px-1 text-sm">${produto.nome}</p>
+    <p class="text-slate-400 px-1 text-xs">Tamanho: M</p>
+    <p class="text-green-700 px-1 font-medium text-lg">$${produto.preco}</p>
+  </div>
+</article>`;
+
+containerProdutoCarrinho.innerHTML += cartaoProdutoCarrinho;
+}
