070d22b36600f5649c63bc873d9e1c207501b884
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-09-08 15:23:22 -0300

    added atualizarPrecoCarrinho :sparkles:

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { renderizarCatalogo } from "./src/cartaoProduto";
 import { inicializarCarrinho} from "./src/menuCarrinho";
-import { adicionarAoCarrinho } from "./src/menuCarrinho";
+// import { adicionarAoCarrinho } from "./src/menuCarrinho";
 
 
 renderizarCatalogo();
 inicializarCarrinho();
-adicionarAoCarrinho();
+// adicionarAoCarrinho();
--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -22,11 +22,13 @@ export function inicializarCarrinho() {
 
 function removerDoCarrinho(idProduto) {
   delete idsProdutoCarrinhoComQuantidade[idProduto];
+  atualizarPrecoCarrinho()
   renderizarProdutosCarrinho();
 }
 
 function incrementarQuantidadeProduto(idProduto) {
   idsProdutoCarrinhoComQuantidade[idProduto]++;
+  atualizarPrecoCarrinho()
   atualizarInformacaoQuantidade(idProduto);
 }
 function decrementarQuantidadeProduto(idProduto) {
@@ -35,11 +37,13 @@ function decrementarQuantidadeProduto(idProduto) {
     return;
   }
   idsProdutoCarrinhoComQuantidade[idProduto]--;
+  atualizarPrecoCarrinho()
   atualizarInformacaoQuantidade(idProduto);
 }
 function atualizarInformacaoQuantidade(idProduto) {
   document.getElementById(`quantidade-${idProduto}`).innerText =
     idsProdutoCarrinhoComQuantidade[idProduto];
+    
 }
 function desenharProdutoNoCarrinho(idProduto) {
   const produto = catalogo.find((p) => p.id === idProduto);
@@ -59,40 +63,41 @@ function desenharProdutoNoCarrinho(idProduto) {
 
   // elementoArticle.classlist.add("flex");
 
-  const { imagem, nome, preco, id } = produto;
   const cartaoProdutoCarrinho = `
-  <button id="remover-item-${id}" class="absolute top-0 right-2">
+  <button id="remover-item-${produto.id}" class="absolute top-0 right-2">
     <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i>
   </button>
   <img
-    src="./assets/img/${imagem}"
-    alt="Carrinho: ${nome}"
+    src="./assets/img/${produto.imagem}"
+    alt="Carrinho: ${produto.nome}"
     title="Carrinho: Camisa Larga com Bolsos"
     class="h-24 rounded-lg"
   />
   <div class="p-2 flex flex-col justify-between">
-    <p class="text-slate-900 px-1 text-sm">${nome}</p>
+    <p class="text-slate-900 px-1 text-sm">${produto.nome}</p>
     <p class="text-slate-400 px-1 text-xs">Tamanho: M</p>
-    <p class="text-green-700 px-1 font-medium text-lg">$${preco}</p>
+    <p class="text-green-700 px-1 font-medium text-lg">$${produto.preco}</p>
   </div>
   <div class="flex text-slate-950 items-end absolute bottom-0 right-2 text-lg">
-    <button id="decrementar-produto-${id}">-</button>
-      <p id="quantidade-${id}" class="ml-2">${idsProdutoCarrinhoComQuantidade[id]}</p>
-    <button id="incrementar-produto-${id}" class="ml-2">+</button>
+    <button id="decrementar-produto-${produto.id}">-</button>
+      <p id="quantidade-${produto.id}" class="ml-2">${
+    idsProdutoCarrinhoComQuantidade[produto.id]
+  }</p>
+    <button id="incrementar-produto-${produto.id}" class="ml-2">+</button>
   </div>
   `;
   elementoArticle.innerHTML = cartaoProdutoCarrinho;
   containerProdutoCarrinho.appendChild(elementoArticle);
 
   document
-    .getElementById(`decrementar-produto-${id}`)
-    .addEventListener("click", () => decrementarQuantidadeProduto(id));
+    .getElementById(`decrementar-produto-${produto.id}`)
+    .addEventListener("click", () => decrementarQuantidadeProduto(produto.id));
   document
-    .getElementById(`incrementar-produto-${id}`)
-    .addEventListener("click", () => incrementarQuantidadeProduto(id));
+    .getElementById(`incrementar-produto-${produto.id}`)
+    .addEventListener("click", () => incrementarQuantidadeProduto(produto.id));
   document
-    .getElementById(`remover-item-${id}`)
-    .addEventListener("click", () => removerDoCarrinho(id));
+    .getElementById(`remover-item-${produto.id}`)
+    .addEventListener("click", () => removerDoCarrinho(produto.id));
 }
 
 function renderizarProdutosCarrinho() {
@@ -101,6 +106,7 @@ function renderizarProdutosCarrinho() {
 
   for (const idProduto in idsProdutoCarrinhoComQuantidade) {
     desenharProdutoNoCarrinho(idProduto);
+    
   }
 }
 
@@ -112,3 +118,16 @@ export function adicionarAoCarrinho(idProduto) {
   idsProdutoCarrinhoComQuantidade[idProduto] = 1;
   desenharProdutoNoCarrinho(idProduto);
 }
+
+function atualizarPrecoCarrinho() {
+  const precoCarrinho = document.getElementById("preco-total");
+  console.log(precoCarrinho)
+
+  let precoTotalCarrinho = 0;
+  for (const idProdutoNoCarrinho in idsProdutoCarrinhoComQuantidade) {
+    precoTotalCarrinho += catalogo.find((p) => p.id === idProdutoNoCarrinho).preco * idsProdutoCarrinhoComQuantidade[idProdutoNoCarrinho];
+    
+  }
+  precoCarrinho.innerText = `Total: $${precoTotalCarrinho}`
+}
+atualizarPrecoCarrinho()
