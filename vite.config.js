65211efe83e1faf904bde60d7c6f0b0d191e0591
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-09-27 17:14:44 -0300

    :start:

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
 // import vueJsxPlugin from "@vitejs/plugin-vue-jsx"
+// import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 export default defineConfig({
-  plugins: [react()],
+  // plugins: [react()],
   base: "/magazine-hashtag",
 });
