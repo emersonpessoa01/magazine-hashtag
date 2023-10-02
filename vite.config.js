b70e4cd2ff98ad311c52319d8e5fa294bdc21972
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-10-02 16:51:35 -0300

    updated viteConfig :sparkles:

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-// import vueJsxPlugin from "@vitejs/plugin-vue-jsx"
-// import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-export default defineConfig({
-  // plugins: [react()],
-  base: "/magazine-hashtag",
-});
+export default{
+  root:".",
+  build:{
+    outDir:"dist",
+  }
+}
